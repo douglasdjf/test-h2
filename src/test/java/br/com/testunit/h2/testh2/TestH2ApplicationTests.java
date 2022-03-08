2d733e098182dcd2d2b03afad3cb73ccdbdf9ab6
Douglas Fonseca <douglas.vasco79@gmail.com>
2022-03-08 03:34:45 -0300

    Definições para valor default dos profiles no maven e pom.xml

--- src/test/java/br/com/testunit/h2/testh2/TestH2ApplicationTests.java
+++ src/test/java/br/com/testunit/h2/testh2/TestH2ApplicationTests.java
@@ -1,13 +1,35 @@
 package br.com.testunit.h2.testh2;
 
+import br.com.testunit.h2.testh2.service.UsuarioService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TestH2ApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	private UsuarioService service;
+
 	@Test
-	void contextLoads() {
+	void deveListarUsuarios() throws Exception {
+
+		mockMvc.perform(get("/usuarios")).andExpect(status().isOk());
+
 	}
 
 }
