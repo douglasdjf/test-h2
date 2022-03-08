c37884bb7a358e9e87deeac949f91a4ccafce1a9
Douglas Fonseca <douglas.vasco79@gmail.com>
2022-03-08 00:58:07 -0300

    Initial commit

---
+++ src/test/java/br/com/testunit/h2/testh2/TestH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.testunit.h2.testh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
